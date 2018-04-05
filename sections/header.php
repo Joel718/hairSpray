6b683e84518f73ad55a54dd505f867acb18c3703
Jonatan Josefsson <jonatanjosef@Jonatans-MacBook-Pro.local>
2018-04-05 15:45:48 +0200

    footer header files

---
+++ sections/header.php
@@ -0,0 +1,21 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>hairMe</h1>
+    
+    <p>Minns du det ljuva 80-talet? Inte jag heller, så låt oss uppleva det. </p>
+</header>
+
+</body>
+</html>
+'
+?>
