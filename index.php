0737bab5157a6c6cfefb4b24aaa56187651a7908
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2018-04-10 12:03:54 +0200

    kundvagn
    Kundvagn slutade funka i föregående version

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<?php include './sections/head_foot.css'; ?>
+
+
 <?php   
+
+
+
 session_start();
     $dbsettings = parse_ini_file('./database.ini');
     $servername = $dbsettings['address'];
@@ -30,8 +36,8 @@ if(isset($_POST["add_to_cart"]))
           }  
           else  
           {  
-     
-              echo '<script>alert("Varukorgen svämmar över")</script>';
+               echo '<script>alert("Item Already Added")</script>';  
+               echo '<script>window.location="index.php"</script>';  
           }  
      }  
      else  
@@ -41,7 +47,6 @@ if(isset($_POST["add_to_cart"]))
                'item_name'               =>     $_POST["hidden_name"],  
                'item_price'          =>     $_POST["hidden_price"],  
                'item_quantity'          =>     $_POST["quantity"]  
-                        
           );  
           $_SESSION["shopping_cart"][0] = $item_array;  
      }  
@@ -55,17 +60,14 @@ if(isset($_GET["action"]))
                if($values["item_id"] == $_GET["id"])  
                {  
                     unset($_SESSION["shopping_cart"][$keys]);  
-                    echo '<script>alert("Vara borttagen")</script>';  
+                    echo '<script>alert("Item Removed")</script>';  
                     echo '<script>window.location="index.php"</script>';  
                }  
           }  
      }  
 }  
 
-
 ?>  
-
-<?php include './sections/header.php';?>
 
 
 <!DOCTYPE html>  
@@ -100,30 +102,26 @@ if(isset($_GET["action"]))
         <h3 align="center">hairSpray</h3><br />
 
         <?php  
-               $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
-               $result = mysqli_query($connect, $query);  
-               if(mysqli_num_rows($result) > 0)  
-               {  
-                    while($row = mysqli_fetch_array($result))  
-                    {  
-               ?>
-        <div class="col-md-4">
-            <form method="post" action="index.php?action=add&id=<?php echo $row[" id "]; ?>">
-                <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
-                    <img src="./bilder/1.jpg" class="img-responsive" /><br />
-                    <h4 class="text-info">
-                        <?php echo $row["name"]; ?>
-                    </h4>
-                    <h4 class="text-danger">$
-                        <?php echo $row["price"]; ?>
-                    </h4>
-                    <input type="text" name="quantity" class="form-control" value="1" />
-                    <input type="hidden" name="hidden_name" value="<?php echo $row[" name "]; ?>" />
-                    <input type="hidden" name="hidden_price" value="<?php echo $row[" price "]; ?>" />
-                    <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
-                </div>
-            </form>
-        </div>
+                $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
+                $result = mysqli_query($connect, $query);  
+                if(mysqli_num_rows($result) > 0)  
+                {  
+                     while($row = mysqli_fetch_array($result))  
+                     {  
+                ?>  
+                <div class="col-md-4">  
+                     <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">  
+                          <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">   
+                               <h4 class="text-info"><?php echo $row["name"]; ?></h4>  
+                               <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
+                               <input type="text" name="quantity" class="form-control" value="1" />  
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
+                               <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
+                               <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
+                          </div>  
+                     </form>  
+                </div>  
+
         <?php  
                     }  
                }  
@@ -146,33 +144,23 @@ if(isset($_GET["action"]))
                               $total = 0;  
                               foreach($_SESSION["shopping_cart"] as $keys => $values)  
                               {  
-                         ?>
-                <tr>
-                    <td>
-                        <?php echo $values["item_name"]; ?>
-                    </td>
-                    <td>
-                        <?php echo $values["item_quantity"]; ?>
-                    </td>
-                    <td>$
-                        <?php echo $values["item_price"]; ?>
-                    </td>
-                    <td>$
-                        <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?>
-                    </td>
-                    <td><a href="index.php?action=delete&id=<?php echo $values[" item_id "]; ?>"><span class="text-danger">Radera</span></a></td>
-                </tr>
-                <?php  
+                         ?>  
+                         <tr>  
+                              <td><?php echo $values["item_name"]; ?></td>  
+                              <td><?php echo $values["item_quantity"]; ?></td>  
+                              <td>$ <?php echo $values["item_price"]; ?></td>  
+                              <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
+                              <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
+                         </tr>  
+                         <?php  
                                    $total = $total + ($values["item_quantity"] * $values["item_price"]);  
                               }  
-                         ?>
-                <tr>
-                    <td colspan="3" align="right">Total</td>
-                    <td align="right">$
-                        <?php echo number_format($total, 2); ?>
-                    </td>
-                    <td></td>
-                </tr>
+                         ?>  
+                         <tr>  
+                              <td colspan="3" align="right">Total</td>  
+                              <td align="right">$ <?php echo number_format($total, 2); ?></td>  
+                              <td></td>  
+                         </tr>  
 
 
                 <?php 
@@ -192,11 +180,9 @@ if(isset($_GET["action"]))
 
                         ?>
 
-
-
                 <?php  
-                            }  
-                            ?>
+                        }  
+                        ?>
                                                 
                     </table>  
                </div>  
@@ -206,7 +192,4 @@ if(isset($_GET["action"]))
      </body>  
 </html>
 
-<?php include './sections/footer.php';?>
-<style>
-<?php include './sections/head_foot.css'; ?>
-</style>
+
