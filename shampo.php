f8619e564b04fafc40f2f55a954e938a427f4390
Jonatan Josefsson <jonatanjosef@Jonatans-MacBook-Pro.local>
2018-04-16 13:34:16 +0200

    new stuff

--- shampo.php
+++ shampo.php
@@ -69,16 +69,16 @@ if(isset($_GET["action"]))
 
 <!DOCTYPE html>  
 <html>  
-     <head>  
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     </head>  
-
-     <body>  
+    <head>  
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://use.fontawesome.com/23e3abc5b7.js"></script>
+    <link rel="stylesheet" href="./sections/head_foot.css">
+    </head>  
+     
+    <body>  
         
 
         <div id="prenumeration">
@@ -91,11 +91,13 @@ if(isset($_GET["action"]))
         </div>
 
         <br />  
-          <div id="navbar" style="font-size: 20px; text-align: center;">
-          <button><a href="vax.php">Vax</a></button>
-          <button><a href="shampo.php">Shampo</a></button>
-          <button><a href="index.php">hairSpray</a></button>
-      </div>
+        <div id="navbar" style="font-size: 20px; text-align: center;">
+        <button><a href="vax.php">Vax</a></button>
+        <button><a href="shampo.php">Shampo</a></button>
+        <button><a href="hairspray.php">hairSpray</a></button>
+        <button><a href="index.php">Alla produkter</a></button>
+        
+    </div>
 
 <body>
     <br />
@@ -123,7 +125,7 @@ if(isset($_GET["action"]))
                 <div class="col-md-4">  
                      <form method="post" action="shampo.php?action=add&id=<?php echo $row["id"]; ?>">  
                           <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">  
-                              <img src="<?php echo $row["image"]; ?>" class="img-responsive" /><br />
+                              <img src="./bilder/<?php echo $row["image"]; ?>" class="img-responsive" /><br />
                                <h4 class="text-info"><?php echo $row["prodname"]; ?></h4>  
                                <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
                                <input type="hidden" name="quantity" class="form-control" value="1" />  
